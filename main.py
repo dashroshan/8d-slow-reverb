6bf58305128e1f8cf54745ee876c8a26dc6ccf34
Roshan Dash <roshan1337d@gmail.com>
2023-03-25 03:06:45 +0530

    comments

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ from effects.effect8d import effect8d
 from effects.slow import effectSlowedDown
 from effects.reverb import effectReverb
 
+# Load the sound file
 sound = loadSound()
+
+# Add 8d effect
 sound8d = effect8d(sound)
+
+# Add slowed down effect
 sound8dAndSlowedDown = effectSlowedDown(sound8d)
+
+# Add reverb effect
 sound8dSlowedDownReverbed, soundSampleRate = effectReverb(sound8dAndSlowedDown)
+
+# Save the sound file
 saveSound(sound8dSlowedDownReverbed, soundSampleRate)
 
 print("8d + slow + reverb effect added successfully!")
